9abc9702e8ca6e63ca627275af78a488242d74fe
Francisco Melicias <melicias1999@gmail.com>
2023-02-07 18:47:04 +0000

    primal thing done good

--- movement/vibratecircle.py
+++ movement/vibratecircle.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 def isPrime(n) :
     if (n <= 1) :
@@ -22,12 +23,36 @@ s = turtle.Screen()
 s.bgcolor("black")
 t.pencolor("green")
 
-a, degree = 1, 90
-t.speed(10)
+size = 100000
+degree = 90
+lastCorner, apoio, countNrCornersPerSize = 1, 1, 0
+t.speed("fastest")
 t.penup()
-t.goto(0,200)
+t.goto(0,0)
 t.pendown()
 
+#disable animation
+s.tracer(0, 0)
+
+for i in range(1,size):
+    print(i)
+    if isPrime(i):
+        t.dot(5, "blue")
+        
+    lastCorner -= 1
+    t.forward(10)
+    
+    if lastCorner == 0:
+        countNrCornersPerSize += 1
+        if countNrCornersPerSize == 2: 
+            apoio += 1
+            countNrCornersPerSize = 0
+        lastCorner = apoio
+        t.left(degree)
+
+#update cause the animation is disabled
+turtle.update()
+"""
 while True:
     print(a/10)
     print(isPrime(a/10))
@@ -42,5 +67,5 @@ while True:
         t.dot(20, "blue")
     a+=10
     t.hideturtle()
-    
+"""
 turtle.done()
